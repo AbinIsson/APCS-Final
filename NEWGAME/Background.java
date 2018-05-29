85b1a1adb69396d21fca3be801b1862bbafac632
AbinIsson <39093227+AbinIsson@users.noreply.github.com>
2018-05-29 17:36:58 -0500

    Update Background.java

--- NEWGAME/Background.java
+++ NEWGAME/Background.java
@@ -1,23 +1,13 @@
 import java.io.File;
-import java.io.FileInputStream;
-
-import javax.print.DocFlavor.URL;
-
-//import sun.audio.*;
 import javafx.application.Application;
-import javafx.beans.property.IntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -25,9 +15,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-//import sun.audio.AudioData;
-//import sun.audio.AudioStream;
  
 public class Background extends Application {
     public static void main(String[] args) {
