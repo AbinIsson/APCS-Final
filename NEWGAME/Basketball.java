309745d08e9a560d564b5051def6c304c76e0fe6
AbinIsson <39093227+AbinIsson@users.noreply.github.com>
2018-05-30 19:06:54 -0500

    Update Basketball.java

--- NEWGAME/Basketball.java
+++ NEWGAME/Basketball.java
@@ -1,12 +1,7 @@
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Bounds;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -18,21 +13,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.PathElement;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Basketball extends Background 
 {
 	private int count = 0;
-	
-	private int score = 1;  // This variable keeps track of the points obtained
-	//  throughout the game
-	private double loc; // location of the ball on the slider 
-	// when stopped during the bar animation
-	private ImageView ball;
+	private int percent = 0; // Stores the percentage of chance for basket
+							//  from each turn of the player. 
 	
 	@Override
 
@@ -46,7 +34,6 @@ public class Basketball extends Background
 
 		StackPane root = new StackPane();
 		
-
 		//Basic set up of Start Page
 		primaryStage.setScene(new Scene(root, 1700, 1000));
 		primaryStage.show();
@@ -67,66 +54,58 @@ public class Basketball extends Background
 		
 
 		  //Bar For Arrow
-	      Rectangle rect1 = new Rectangle(20, 20, 100, 50);
-       		rect1.setFill(Color.BLUE);
-        rect1.setTranslateX(10);
-        rect1.setTranslateY(400);
-        root.getChildren().addAll(rect1);
+	     	//Bar For Arrow
+		Rectangle rect1 = new Rectangle(20, 20, 100, 50);
+		rect1.setFill(Color.BLUE);
+		rect1.setTranslateX(10);
+		rect1.setTranslateY(400);
+		root.getChildren().addAll(rect1);
+
+
+		Rectangle rect2 = new Rectangle(20, 20, 70, 50);
+		rect2.setFill(Color.RED);
+		rect2.setTranslateX(90);
+		rect2.setTranslateY(400);
+		root.getChildren().addAll(rect2);
+
+		Rectangle rect3 = new Rectangle(20, 20, 40, 50);
+		rect3.setFill(Color.ORANGE);
+		rect3.setTranslateX(140);
+		rect3.setTranslateY(400);
+		root.getChildren().addAll(rect3);
+
+		Rectangle rect4 = new Rectangle(20, 20, 24, 50);
+		rect4.setFill(Color.GREEN);
+		rect4.setTranslateX(165);
+		rect4.setTranslateY(400);
+		root.getChildren().addAll(rect4);
+
+		Rectangle rect5 = new Rectangle(20, 20, 40, 50);
+		rect5.setFill(Color.ORANGE);
+		rect5.setTranslateX(195);
+		rect5.setTranslateY(400);
+		root.getChildren().addAll(rect5);
+
+		Rectangle rect6 = new Rectangle(20, 20, 70, 50);
+		rect6.setFill(Color.RED);
+		rect6.setTranslateX(242);
+		rect6.setTranslateY(400);
+		root.getChildren().addAll(rect6);
+
+		Rectangle rect7 = new Rectangle(20, 20, 100, 50);
+		rect7.setFill(Color.BLUE);
+		rect7.setTranslateX(325);
+		rect7.setTranslateY(400);
+		root.getChildren().addAll(rect7);
         
-        
-        Rectangle rect2 = new Rectangle(20, 20, 70, 50);
-        rect2.setFill(Color.RED);
-        rect2.setTranslateX(90);
-        rect2.setTranslateY(400);
-        root.getChildren().addAll(rect2);
-        
-        Rectangle rect3 = new Rectangle(20, 20, 40, 50);
-        rect3.setFill(Color.ORANGE);
-        rect3.setTranslateX(140);
-        rect3.setTranslateY(400);
-        root.getChildren().addAll(rect3);
-        
-        Rectangle rect4 = new Rectangle(20, 20, 24, 50);
-        rect4.setFill(Color.GREEN);
-        rect4.setTranslateX(165);
-        rect4.setTranslateY(400);
-        root.getChildren().addAll(rect4);
-        
-        Rectangle rect5 = new Rectangle(20, 20, 40, 50);
-        rect5.setFill(Color.ORANGE);
-        rect5.setTranslateX(195);
-        rect5.setTranslateY(400);
-        root.getChildren().addAll(rect5);
-      
-        Rectangle rect6 = new Rectangle(20, 20, 70, 50);
-        rect6.setFill(Color.RED);
-        rect6.setTranslateX(242);
-        rect6.setTranslateY(400);
-        root.getChildren().addAll(rect6);
-        
-        Rectangle rect7 = new Rectangle(20, 20, 100, 50);
-        rect7.setFill(Color.BLUE);
-        rect7.setTranslateX(325);
-        rect7.setTranslateY(400);
-        root.getChildren().addAll(rect7);
-        
-        gc.setFill(Color.BLACK);
-		gc.fillRect(1100, 50, 400, 200);
-		
-		gc.setFill(Color.RED);
-		gc.setStroke( Color.BLACK );
-		gc.setLineWidth(3);
-		Font theFont = Font.font( "Verdana", FontWeight.BOLD, 35 );
-		gc.setFont( theFont );
-		gc.fillText( "Score", 1285, 44 );
-		gc.strokeText( "Score", 1285, 44);
+    
 		
 		
 
-        ImageView ball2 = new ImageView(); 	//Ball on Bar
+       		ImageView ball2 = new ImageView(); 	//Ball on Bar
 		ball2.setImage(new Image("file:Ball.png",40, 40, false, false));      
-        
-        PathElement[] bar = Animation.bar();
+        	
+       		PathElement[] bar = Animation.bar();
 
 
 		//Adding location of the path for animation
@@ -145,48 +124,69 @@ public class Basketball extends Background
 		
 		root.getChildren().addAll(road2,ball2);
 		
-		
-		/*
-		 * use getScore() to display the score after the box is done 
-		 */
-		
 		Button stopButton = new Button();
 		stopButton.setText("Stop!!!");
 		stopButton.setTranslateX(-400);
 		stopButton.setTranslateY(400);
 		stopButton.setMaxSize(300, 100);
+		
 		root.getChildren().add(stopButton);
 		TryAgain again = new TryAgain();
 	      
 		Stage cool = new Stage();
 	     
-	        stopButton.setOnAction(new EventHandler<ActionEvent>() {
+	        stopButton.setOnAction(new EventHandler<ActionEvent>() 
+		{
 	            @Override public void handle(ActionEvent event)
 	            {
 	            	barAnim.pause();
 	            	
-	            	//Getting a random path animation
-	            	PathElement[] path = Animation.getRandomAnimation((int) (Math.random()*5+1));
-	            	 
+	            	percent = getPercent(ball.getTranslateX());
+	            	
+	            	if(percent > 20)
+	            	{
+	            		//Getting a random path animation
+		            	PathElement[] path = Animation.getRandomAnimation((int) (Math.random()*5+1));
+		            	 
 
-	        		//Adding location of the path for animation
-	        		Path road = new Path();
-	        		road.setStroke(Color.TRANSPARENT); //Making the color invisible
-	        		road.getElements().addAll(path);
+		        		//Adding location of the path for animation
+		        		Path road = new Path();
+		        		road.setStroke(Color.TRANSPARENT); //Making the color invisible
+		        		road.getElements().addAll(path);
 
-	        		PathTransition anim = new PathTransition();
-	        		anim.setNode(ball);
-	        		anim.setPath(road);
-	        		anim.setDuration(new Duration(1500));
-	        		anim.setCycleCount(Timeline.INDEFINITE);
-	        		
-	        		/*Canvas canvas = new Canvas( 1700, 1000 );
-	        		root.getChildren().add( canvas );*/
-	        		root.getChildren().addAll(road,ball);
-	        		
-	            	//PLAY ANIMATION COMMAND
-	        		anim.play();
-	          
+		        		PathTransition anim = new PathTransition();
+		        		anim.setNode(ball);
+		        		anim.setPath(road);
+		        		anim.setDuration(new Duration(1500));
+		        		anim.setCycleCount(Timeline.INDEFINITE);
+		        
+		        		root.getChildren().addAll(road,ball);
+		        		
+		            	//PLAY ANIMATION COMMAND
+		        		anim.play();
+	            	}
+	            	else
+	            	{
+	            		//Getting a random path animation
+		            	PathElement[] path = Animation.getMissAnimation((int) (Math.random()*4+1));
+		            	 
+
+		        		//Adding location of the path for animation
+		        		Path road = new Path();
+		        		road.setStroke(Color.TRANSPARENT); //Making the color invisible
+		        		road.getElements().addAll(path);
+
+		        		PathTransition anim = new PathTransition();
+		        		anim.setNode(ball);
+		        		anim.setPath(road);
+		        		anim.setDuration(new Duration(1500));
+		        		anim.setCycleCount(Timeline.INDEFINITE);
+		        
+		        		root.getChildren().addAll(road,ball);
+		        		
+		            	//PLAY ANIMATION COMMAND
+		        		anim.play();
+	            	}
 				
 	        
 	            	Stage primaryStage = new Stage();
@@ -197,46 +197,17 @@ public class Basketball extends Background
 	            		
 	            	}
 	            	
-	            	ChangeListener<Number> listener = new ChangeListener<Number>() {
-	            		@Override
-	            		public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
-	            			Bounds boundsInScene = ball.localToScene(ball.getBoundsInLocal());
-	            			double xInScene = boundsInScene.getMinX();
-	            			double yInScene = boundsInScene.getMinY();
-	            			// do something with values...
-	            		}
-	            	};
-	        ball.translateXProperty().addListener(listener);
-	        ball.translateYProperty().addListener(listener);
-	        
-	        calculateScore(ball.getTranslateX());
-	        gc.setFill(Color.RED);
-			gc.setStroke( Color.BLACK );
-			gc.setLineWidth(3);
-		
-			gc.setFont( theFont );
-			gc.fillText( getScore(), 1285, 180 );
-			gc.strokeText( getScore(), 1285, 180);
-	           
+			}
+			}};
 	            	
-	            }
-	            
-	        });
-	          gc.setFill(Color.ALICEBLUE);
-			gc.setStroke(Color.BLACK);
-			gc.setLineWidth(2);
-			Font daFont = Font.font("Arial", FontWeight.SEMI_BOLD, 25);
-			gc.setFont(daFont);
-			gc.fillText(getScore().toString() + " ",550 , 65);
-			gc.strokeText(getScore().toString(), 550, 65);
+	       
 		
-	        Button tryAgain = new Button ("Try Again");
-	        
-	        
-	        tryAgain.setTranslateX(-400);
-			tryAgain.setTranslateY(300);
-			tryAgain.setMaxSize(300, 100);
-			root.getChildren().add(tryAgain);
+	       Button tryAgain = new Button ("Try Again (Only Press Once)");
+	       tryAgain.setTranslateX(-400);
+	       tryAgain.setTranslateY(300);
+	       tryAgain.setMaxSize(300, 100);
+	       
+	        root.getChildren().add(tryAgain);
 	        count++;
         	if(count != 1)
         	{
@@ -269,60 +240,30 @@ public class Basketball extends Background
 	}
 	
 	/*
-	 * @param - location of the ball on the rectangle, 
-	 * this will determine the score for that player
+	 * @param -  location of the ball on the rectangle, 
+	 * this will determine overall chance of it being a make
 	 */
-	private void calculateScore(double location)
+	private int getPercent(double location)
 	{
-		if(location < 100)
+		int percent = 0;
+		if(location < 100 & location > 70)
 		{
-	
-	
+			percent += 25;
 			
-			
-			
-		}
-		
-		else if(location < 70)
-		{
-			incrementScore(1);
-			
-			/*else if(location < 40)
+			if(location < 70 & location > 40)
 			{
-				incrementScore(2);
+				percent += 30;
 				
-				else if(location < 24)
+				if(location < 40)
 				{
-					incrementScore(3);
+					percent += 45;
 				}
-			}*/
-		}
-		else if(location < 40)
-		{
-			incrementScore(2);
+			}
 			
-		
 		}
+		
+		return percent;
 	}
-
-	/*
-	 * @param - the number of points that  needs to be added
-	 * Updates the score variable with @param
-	 */
-	private void incrementScore(int howMany)
-	{
-		score += howMany;
-	}
-
-	/*
-	 * @return the updated the score of the current player
-	 */
-	private String getScore()
-	{
-		return ""+score;
-	}
-
-
 
 
 } 
